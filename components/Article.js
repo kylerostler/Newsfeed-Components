8d07e3382d7fde593517b0a4333422ad19cc5ecf
kylerostler <93017344+kylerostler@users.noreply.github.com>
2021-12-01 13:19:30 -0700

    started making component

--- components/Article.js
+++ components/Article.js
@@ -94,6 +94,7 @@ const data = [
   Your component is a function that takes an article object as its only argument,
   and returns a DOM node looking like the one below:
 
+
   <div class="article">
     <h2>{title of the article}</h2>
     <p class="date">{date of the article}</p>
@@ -102,15 +103,39 @@ const data = [
 
     <span class="expandButton">+</span>
   </div>
+*/
+function articleMaker(obj){
+  const article = document.createElement('div');
+  const articleTitle = document.createElement('h2');
+  const paraOne = document.createElement('p');
+  const paraTwo = document.createElement('p');
+  const paraThree = document.createElement('p');
+  const expandButton = document.createElement('span');
 
+  expandButton.addEventListener('click', () =>{
+    article.classList.toggle('article-open');
+  });
+  return article;
+};
+
+/*
   Step 2: Still inside `articleMaker`, add an event listener to the span.expandButton.
   This listener should toggle the class 'article-open' on div.article.
+*/
 
+/*
   Step 3: Don't forget to return something from your function!
+*/
 
+/*
   Step 4: Outside your function now, loop over the data. At each iteration you'll use your component
   to create a div.article element and append it to the DOM inside div.articles (see index.html).
+*/
+const articleElements = data.forEach(elem => {
+  
+});
 
+/*
   Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
   Refresh the page to see the new article.
 */
